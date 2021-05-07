77cc5d5b1772481a0279eec73089ccd9d85bca77
CoRob <71831663+Corentin-Robac@users.noreply.github.com>
2021-05-07 18:31:03 +0200

    Retirer fetch pour fiche produit
    Remplacer le fetch de la fiche produit par localstorage, mise en place de l'incrémentation de la quantité des produits du panier avec localstorage, retirer option "choisir" du modèle de lentille

--- front/js/index.js
+++ front/js/index.js
@@ -1,10 +1,16 @@
 main();
 
 async function main() {
+
+    localStorage.removeItem('cameras');
+    tab_cameras = [];
     const cameras = await getCameras();
     for (camera of cameras) {
         displayCamera(camera);
+        localstorage(camera,tab_cameras);
     }
+
+    localStorage.setItem('cameras', JSON.stringify(tab_cameras));
 }
 
 function getCameras() {
@@ -20,5 +26,18 @@ function getCameras() {
 function displayCamera(camera) {
     let price = (camera.price/100).toFixed(2);
     document.getElementById("card-display-shop").innerHTML += '<a href="template_product.html?'+camera._id+'"<div class="col-xs-12 col-md-6 col-lg-4 mt-4 card-camera" ><div class="card shadow"><img class="card-img-top" src="'+camera.imageUrl+'" alt="Card image cap"><div class="card-body"><p class="card-title" id="cameraname">'+camera.name+'</p><p class="card-text">'+camera.description+'</p><p id="cameraprice">'+price+' €'+'</p></div></div></div></a>';
+}
 
+function localstorage(camera,tab_cameras){
+    //creer un objet avec les infos de la camera
+    infosCamera = {
+        id: camera._id,
+        name: camera.name,
+        price: camera.price,
+        image: camera.imageUrl,
+        modeles: camera.lenses,
+        description: camera.description
+    };
+    //rentrer cet objet dans le tableau
+    tab_cameras.push(infosCamera);
 }
--- front/js/template_product.js
+++ front/js/template_product.js
@@ -1,33 +1,36 @@
 main();
 
 async function main() {
-    const cameras = await getCameras();
+    let camera_localStorage = JSON.parse(localStorage.getItem("cameras"));
     const id_product = await getId();
-    for (camera of cameras) {
-        if(camera._id == id_product){
-        displayCamera(camera);
+    let objetCamera;
+    objetCamera = getCamera(camera_localStorage,id_product);
+    displayCamera(objetCamera);
+}
+
+function getCamera(camera_localStorage,id_product) {
+    for (camera of camera_localStorage) {
+        if(camera.id == id_product){
+            objetCamera = {
+                id: camera.id,
+                name: camera.name,
+                price: camera.price,
+                image: camera.image,
+                modeles: camera.modeles,
+                description: camera.description
+            };
         }
     }
+    return objetCamera;
 }
 
-function getCameras() {
-    return fetch("http://localhost:3000/api/cameras")
-    .then(function(resultBody) {
-        return resultBody.json();
-    })
-    .catch(function(error) {
-        alert(error);
-    })
-}
-
-function displayCamera(camera) {
-    document.getElementById("cameraimage").innerHTML += '<img class="card-img-top img-fluid" src="'+camera.imageUrl+'" alt="Image du produit">';
-    document.getElementById("cameraname").innerHTML += camera.name;
-    let price = (camera.price/100).toFixed(2);
+function displayCamera(objetCamera) {
+    document.getElementById("cameraimage").innerHTML += '<img class="card-img-top img-fluid" src="'+objetCamera.image+'" alt="Image du produit">';
+    document.getElementById("cameraname").innerHTML += objetCamera.name;
+    let price = (objetCamera.price/100).toFixed(2);
     document.getElementById("cameraprice").innerHTML += price+'€';
-    document.getElementById("cameradescription").innerHTML += camera.description;
-
-    modeles = camera.lenses;
+    document.getElementById("cameradescription").innerHTML += objetCamera.description;
+    modeles = objetCamera.modeles;
     for (modele of modeles){
         document.getElementById("inlineFormCustomSelect").innerHTML += '<option value="'+modele+'">'+modele+'</option>';
     }
@@ -39,8 +42,10 @@ function getId(){
     return id_product; //Retourne l'id
 }
 
+
+/*----------Ajouter au panier----------*/
 function setLocalStorage(infosCamera) {
-    let camera_localStorage = JSON.parse(localStorage.getItem("camera"));
+    let camera_localStorage = JSON.parse(localStorage.getItem("panier"));
     if(camera_localStorage){
         add_to_localStorage(infosCamera,camera_localStorage);
     }
@@ -53,39 +58,39 @@ function setLocalStorage(infosCamera) {
 function add_to_localStorage(infosCamera,camera_localStorage){
     let flag = 0;
     //si id et modele de infosCamera = id et modele d'un des objets de camera_localStorage alors quantité de cet objet += quantité infosCamera
-    console.log(camera_localStorage);
     for (camera_in_local of camera_localStorage){
         if(camera_in_local.id == infosCamera.id && camera_in_local.modele == infosCamera.modele){
-            //supprimer l'objet du local storage
-            parseFloat(camera_in_local.quantity) += parseFloat(infosCamera.quantity); // 2 à la place de 11 --> ParseFloat();
+            let quantity_local = parseFloat(camera_in_local.quantity);
+            let quantity_choosed = parseFloat(infosCamera.quantity);
+            quantity_local += quantity_choosed;
+            camera_in_local.quantity = quantity_local;
             flag = 1;
         }
     }
     if(flag == 0){
         camera_localStorage.push(infosCamera);
-        localStorage.setItem("camera", JSON.stringify(camera_localStorage));
     }
+    localStorage.setItem("panier", JSON.stringify(camera_localStorage));
 }
 
 let add_to_cart = document.querySelector("#button");
 add_to_cart.addEventListener("click", async function() {
     modele_selected = document.querySelector("#inlineFormCustomSelect").value;
     quantity_selected = document.querySelector("#quantity").value;
-    const cameras = await getCameras();
+    let camera_localStorage = JSON.parse(localStorage.getItem("cameras"));
     const id_product = await getId();
-    for (camera of cameras) {
-        if(camera._id == id_product){
-            infosCamera = {
-                id: camera._id,
-                name: camera.name,
-                price: camera.price,
-                image: camera.imageUrl,
-                description: camera.description,
-                modele: modele_selected,
-                quantity: quantity_selected
-            };
-        }
-    }
+    const camera = await getCamera(camera_localStorage,id_product);
+        infosCamera = {
+            id: camera.id,
+            name: camera.name,
+            price: camera.price,
+            image: camera.image,
+            description: camera.description,
+            modele: modele_selected,
+            quantity: quantity_selected
+        };
     setLocalStorage(infosCamera);
 });
+/*----------FIN Ajouter au panier----------*/
+
 
